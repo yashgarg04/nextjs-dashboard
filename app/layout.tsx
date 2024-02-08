c15733a477d02aff9be558095bc48addaf4674b4
yashgarg04 <yashgarg0408@gmail.com>
2024-02-08 17:11:23 +0530

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 import { Metadata } from 'next';
  
@@ -19,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
